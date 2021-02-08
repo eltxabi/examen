2632c0406cfa32f3782dfa99fc26b17602ddfa59
javiermmIBQ <javiermm@educastur.org>
2021-02-08 20:24:02 +0100

    Inicio

---
+++ WebContent/index.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ page import="java.util.HashMap" %>
+   
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Examen</title>
+</head>
+<body>
+<table> 
+   		<tr> 
+          <th><b>Dato</b></th> 
+          <th><b>Valor</b></th> 
+        </tr>
+         <% HashMap<String,String> datos=(HashMap<String,String>)request.getAttribute("datos"); 
+	    	if (datos != null){	
+	    		for (String i : datos.keySet()) {%> 
+	            <tr> 
+	                <td><%=i%></td> 
+	                <td><%=datos.get(i)%></td> 
+	            </tr> 
+              <%}
+	         }%> 
+    </table>  
+
+</body>
+</html>
---
+++ src/main/Datosred.java
@@ -0,0 +1,73 @@
+package main;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.TimeZone;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Datosred
+ */
+@WebServlet("")
+public class Datosred extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Datosred() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		 HashMap<String, String> datos = new HashMap<String, String>();
+		 RequestDispatcher despachador = null;
+		 InetAddress ip;
+		 ip = InetAddress.getLocalHost();
+		 Calendar c = Calendar.getInstance();
+		 TimeZone tz = c.getTimeZone();
+		 Date date = new Date();
+	
+		 datos.put("HostName", ip.getHostName());
+		 datos.put("HostAddress", ip.getHostAddress());
+		 datos.put("OS Name", System.getProperty("os.name").toLowerCase());
+		 datos.put("OS Arch", System.getProperty("os.arch").toLowerCase());
+		 datos.put("OS Version", System.getProperty("os.version").toLowerCase());
+		 datos.put("Date", date.toString());
+		 datos.put("TimeZone", tz.getDisplayName());
+		 datos.put("User Name", System.getProperty("user.name").toLowerCase());
+		 datos.put("User Home", System.getProperty("user.home").toLowerCase());
+		 datos.put("User Dir", System.getProperty("user.dir").toLowerCase());
+		 datos.put("Java Vendor", System.getProperty("java.vendor").toLowerCase());
+		 datos.put("Java Version", System.getProperty("java.version").toLowerCase());
+		
+		 
+		 request.setAttribute("datos",datos);
+		 despachador = request.getServletContext().getRequestDispatcher("/index.jsp");
+		 despachador.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
