c922c111f6c42da937d0be9f3113d069a1de4bea
Javier <you@example.com>
2021-02-08 23:15:13 +0100

    prueba10

--- src/main/Datosred.java
+++ src/main/Datosred.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Datosred
  */
-@WebServlet("")
+@WebServlet("/")
 public class Datosred extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
